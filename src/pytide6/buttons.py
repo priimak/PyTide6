b2770f617671aebff698fd4ac03fce167c685222
Dmitri Priimak <priimak@gmail.com>
2024-12-24 13:11:55 -0600

    PushButton::on_clicked allows return Any.

--- src/pytide6/buttons.py
+++ src/pytide6/buttons.py
@@ -1,10 +1,10 @@
-from typing import Callable
+from typing import Callable, Any
 
 from PySide6.QtWidgets import QPushButton, QWidget
 
 
 class PushButton(QPushButton):
-    def __init__(self, text: str, parent: QWidget | None = None, on_clicked: Callable[[], None] | None = None):
+    def __init__(self, text: str, parent: QWidget | None = None, on_clicked: Callable[[], Any] | None = None):
         super().__init__(text, parent)
         if on_clicked is not None:
             self.clicked.connect(on_clicked)
