7bca643099207f83e4c4eb9671d9cdd10def1980
Dmitri Priimak <priimak@gmail.com>
2025-01-09 16:22:36 -0600

    PushButton now accepts css and cursor args.

--- src/pytide6/buttons.py
+++ src/pytide6/buttons.py
@@ -1,5 +1,6 @@
 from typing import Callable, Any
 
+import PySide6
 from PySide6.QtWidgets import QPushButton, QWidget
 
 
@@ -9,10 +10,18 @@ class PushButton(QPushButton):
                  parent: QWidget | None = None,
                  *,
                  on_clicked: Callable[[], Any] | None = None,
-                 auto_default: bool | None = None):
+                 auto_default: bool | None = None,
+                 style_sheet: str | None = None,
+                 cursor: PySide6.QtGui.QCursor | PySide6.QtCore.Qt.CursorShape | PySide6.QtGui.QPixmap | None = None):
         super().__init__(text, parent)
         if on_clicked is not None:
             self.clicked.connect(on_clicked)
 
         if auto_default is not None:
             self.setAutoDefault(auto_default)
+
+        if style_sheet is not None:
+            self.setStyleSheet(style_sheet)
+
+        if cursor is not None:
+            self.setCursor(cursor)
