e0aa136906d332f30fd1aa9c80e26f25b9696d5b
Dmitri Priimak <priimak@gmail.com>
2025-01-09 16:19:21 -0600

    PushButton now accepts auto_default arg.

--- src/pytide6/buttons.py
+++ src/pytide6/buttons.py
@@ -4,7 +4,15 @@ from PySide6.QtWidgets import QPushButton, QWidget
 
 
 class PushButton(QPushButton):
-    def __init__(self, text: str, parent: QWidget | None = None, on_clicked: Callable[[], Any] | None = None):
+    def __init__(self,
+                 text: str,
+                 parent: QWidget | None = None,
+                 *,
+                 on_clicked: Callable[[], Any] | None = None,
+                 auto_default: bool | None = None):
         super().__init__(text, parent)
         if on_clicked is not None:
             self.clicked.connect(on_clicked)
+
+        if auto_default is not None:
+            self.setAutoDefault(auto_default)
