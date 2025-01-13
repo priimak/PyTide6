b7c95305d6dceb7ed6231bd51a5833737c41cd17
Dmitri Priimak <priimak@gmail.com>
2025-01-13 12:05:50 -0600

    Added __match_args__ to class W.

--- src/pytide6/widget_wrapper.py
+++ src/pytide6/widget_wrapper.py
@@ -3,6 +3,8 @@ from PySide6.QtWidgets import QWidget
 
 
 class W:
+    __match_args__ = ("widget", "stretch", "alignment")
+
     def __init__(self, widget: QWidget, stretch: int = 0, alignment: Qt.AlignmentFlag = 0):
         self.widget = widget
         self.stretch = stretch
