7c3266174301fa2f4075b107ba5be5204cc7fbb2
Dmitri Priimak <priimak@gmail.com>
2025-01-19 13:19:14 -0600

    Added action callback to LineTextInput cons

--- src/pytide6/inputs.py
+++ src/pytide6/inputs.py
@@ -1,3 +1,5 @@
+from collections.abc import Callable
+
 from PySide6.QtWidgets import QLabel, QLineEdit
 
 from pytide6.layout import HBoxLayout
@@ -5,13 +7,17 @@ from pytide6.panel_widget import Panel
 
 
 class LineTextInput(Panel[HBoxLayout]):
-    def __init__(self, label: str | None, text: str = ""):
+    def __init__(self, label: str | None, text: str = "", on_text_change: Callable[[str], None] | None = None):
         super().__init__(HBoxLayout())
 
         if label is not None:
             self.addWidget(QLabel(label))
 
         self._input = QLineEdit(text)
+
+        if on_text_change is not None:
+            self._input.textChanged.connect(on_text_change)
+
         self.addWidget(self._input)
 
     def text(self) -> str:
