ddcb541ae9c787a490ca4803b8e5fbf41e1a57d8
Dmitri Priimak <priimak@gmail.com>
2025-01-19 10:53:44 -0600

    Added action callback to CheckBox cons

--- src/pytide6/check_box.py
+++ src/pytide6/check_box.py
@@ -1,7 +1,20 @@
+from collections.abc import Callable
+
 from PySide6.QtWidgets import QCheckBox
 
 
 class CheckBox(QCheckBox):
-    def __init__(self, text: str, parent = None, *, checked: bool = False):
+    def __init__(self,
+                 text: str,
+                 parent = None,
+                 *,
+                 checked: bool = False,
+                 on_change: Callable[[bool], None] = lambda _: None):
         super().__init__(text, parent)
+        self.__on_change = on_change
         self.setChecked(checked)
+
+        self.clicked.connect(self._dispatch)
+
+    def _dispatch(self):
+        self.__on_change(self.isChecked())
