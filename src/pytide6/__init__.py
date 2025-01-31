d6bcf372c90125466f259aad9b74489d2ad417fc
Dmitri Priimak <priimak@gmail.com>
2025-01-31 09:02:22 -0600

    Better imports

--- src/pytide6/__init__.py
+++ src/pytide6/__init__.py
@@ -1,3 +1,4 @@
+from .buttons import PushButton
 from .check_box import CheckBox
 from .combo_box import ComboBox
 from .dialogs import Dialog
@@ -8,4 +9,5 @@ from .layout import Layout, HBoxLayout, VBoxLayout
 from .open_file_protocol import FileOpener
 from .panel_widget import Panel, HBoxPanel, VBoxPanel
 from .recenetly_opened_files import init_last_opened_menu, update_last_opened_files_menu
+from .widget_wrapper import W
 from .windows import MainWindow
