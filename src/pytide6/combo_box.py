107a684d62fe483ff4fdf19c3a47075f06459d56
Dmitri Priimak <priimak@gmail.com>
2025-05-04 10:56:51 -0500

    on_focus(...) takes QFocusEvent argument

--- src/pytide6/combo_box.py
+++ src/pytide6/combo_box.py
@@ -1,5 +1,6 @@
 from collections.abc import Callable
 
+from PySide6.QtGui import QFocusEvent
 from PySide6.QtWidgets import QComboBox
 
 
@@ -11,7 +12,7 @@ class ComboBox(QComboBox):
                  items: list[str] | None = None,
                  current_selection: int | str | None = None,
                  on_text_change: Callable[[str], None] | None = None,
-                 on_focus: Callable[[], None] = lambda: None) -> None:
+                 on_focus: Callable[[QFocusEvent], None] = lambda _: None) -> None:
         super(ComboBox, self).__init__(parent)
 
         if min_width is not None:
@@ -33,4 +34,4 @@ class ComboBox(QComboBox):
 
     def focusInEvent(self, e):
         super().focusInEvent(e)
-        self.on_focus()
+        self.on_focus(e)
