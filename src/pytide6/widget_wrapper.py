1690d987f5a025c490fc735f22060805a68114fa
Dmitri Priimak <priimak@gmail.com>
2026-08-05 12:55:29 -0500

    class W now used QWidget() as default in cons

--- src/pytide6/widget_wrapper.py
+++ src/pytide6/widget_wrapper.py
@@ -5,7 +5,12 @@ from PySide6.QtWidgets import QWidget
 class W:
     __match_args__ = ("widget", "stretch", "alignment")
 
-    def __init__(self, widget: QWidget, stretch: int = 0, alignment: Qt.AlignmentFlag | None = None):
-        self.widget = widget
+    def __init__(
+        self,
+        widget: QWidget | None = None,
+        stretch: int = 0,
+        alignment: Qt.AlignmentFlag | None = None,
+    ):
+        self.widget = QWidget() if widget is None else widget
         self.stretch = stretch
         self.alignment = alignment
