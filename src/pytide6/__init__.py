5662346dcaa98ab79cdcded3df1fcd7c0527f0e7
Dmitri Priimak <priimak@gmail.com>
2026-08-08 08:25:08 -0500

    Added Space to __init__.py

--- src/pytide6/__init__.py
+++ src/pytide6/__init__.py
@@ -5,7 +5,7 @@ from .dialogs import Dialog
 from .geometry import set_geometry
 from .inputs import LineTextInput
 from .labels import Label, RichTextLabel
-from .layout import Layout, HBoxLayout, VBoxLayout
+from .layout import Layout, HBoxLayout, VBoxLayout, Spacer
 from .open_file_protocol import FileOpener
 from .panel_widget import Panel, HBoxPanel, VBoxPanel
 from .recenetly_opened_files import init_last_opened_menu, update_last_opened_files_menu
