dcbf9d1f069adbeaaf2e3ed24698632af12be625
Dmitri Priimak <priimak@gmail.com>
2026-09-11 09:11:44 -0500

    Added Prompt class to __init__.py

--- src/pytide6/__init__.py
+++ src/pytide6/__init__.py
@@ -1,15 +1,15 @@
 from .buttons import PushButton
 from .check_box import CheckBox
 from .combo_box import ComboBox
-from .dialogs import Dialog
+from .dialogs import Dialog, Prompt
 from .geometry import set_geometry
 from .inputs import LineTextInput
 from .labels import Label, RichTextLabel
-from .layout import Layout, HBoxLayout, VBoxLayout, Spacer
+from .layout import HBoxLayout, Layout, Spacer, VBoxLayout
+from .menu import Menu, MenuSeparator
 from .open_file_protocol import FileOpener
-from .panel_widget import Panel, HBoxPanel, VBoxPanel
+from .panel_widget import HBoxPanel, Panel, VBoxPanel
 from .recenetly_opened_files import init_last_opened_menu, update_last_opened_files_menu
+from .splitter import Splitter
 from .widget_wrapper import W
 from .windows import MainWindow
-from .splitter import Splitter
-from .menu import Menu, MenuSeparator
